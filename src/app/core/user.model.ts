88bb19cfccf77dc6725808f9184a7a84bdc825bb
Caren <karen.rizk1997@gmail.com>
2020-01-25 18:57:18 -0500

    edit profile information

--- src/app/core/user.model.ts
+++ src/app/core/user.model.ts
@@ -2,10 +2,12 @@ export class FirebaseUserModel {
   image: string;
   name: string;
   provider: string;
+  email: string;
 
   constructor(){
     this.image = "";
     this.name = "";
     this.provider = "";
+    this.email = "";
   }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -5,6 +5,8 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FirebaseUserModel } from '../core/user.model';
+import { MatDialog } from '@angular/material/dialog';
+import { EditDialogComponent } from './edit-dialog/edit-dialog.component';
 
 @Component({
   selector: 'page-user',
@@ -15,38 +17,45 @@ export class UserComponent implements OnInit{
 
   user: FirebaseUserModel = new FirebaseUserModel();
   profileForm: FormGroup;
+  editState: boolean;
 
   constructor(
     public userService: UserService,
     public authService: AuthService,
     private route: ActivatedRoute,
     private location : Location,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    public dialog: MatDialog
   ) {
 
   }
 
   ngOnInit(): void {
+    this.editState = false;
     this.route.data.subscribe(routeData => {
       let data = routeData['data'];
       if (data) {
         this.user = data;
-        this.createForm(this.user.name);
+        this.createForm(this.user.name, this.user.email);
       }
     })
   }
 
-  createForm(name) {
+  createForm(name: string, email?: string) {
     this.profileForm = this.fb.group({
-      name: [name, Validators.required ]
+      name: [name, Validators.required ],
+      email: [email]
     });
+    this.profileForm.get('name').disable();
+    this.profileForm.get('email').disable();
   }
 
   save(value){
+    this.editToggle();
     this.userService.updateCurrentUser(value)
     .then(res => {
       console.log(res);
-    }, err => console.log(err))
+    }, err => console.error(err))
   }
 
   logout(){
@@ -55,6 +64,25 @@ export class UserComponent implements OnInit{
       this.location.back();
     }, (error) => {
       console.log("Logout error", error);
+    });
+  }
+
+  editToggle() {
+    this.editState = !this.editState;
+
+    let control = this.profileForm.get('name')
+    control.disabled ? control.enable() : control.disable();
+    control = this.profileForm.get('email')
+    control.disabled ? control.enable() : control.disable();
+  }
+
+  openDialog(user: FirebaseUserModel): void {
+    const dialogRef = this.dialog.open(EditDialogComponent, {
+      data: { user: user }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed', result);
     });
   }
 }
