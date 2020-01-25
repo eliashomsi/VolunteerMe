50995e9c71d5740426d59cbe144fe6349a529620
Elias Al Homsi <elias.alhomsi@mail.mcgill.ca>
2020-01-25 16:29:33 -0500

    Add Dynamic project list

--- src/app/project-list/project-list.component.ts
+++ src/app/project-list/project-list.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { ProjectModel} from '../core/project.model'
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-project-list',
@@ -9,10 +11,19 @@ import { ProjectModel} from '../core/project.model'
   styleUrls: ['./project-list.component.scss']
 })
 export class ProjectListComponent {
-  public projects: AngularFireList<ProjectModel>;
+  public projectsRef: AngularFireList<ProjectModel>;
+  public projects$: Observable<ProjectModel[]>;
+  public projects: ProjectModel[];
 
   constructor(db: AngularFireDatabase) {
-    this.projects = db.list('/projects');
+    this.projectsRef = db.list('/projects');
+    this.projects$ = this.projectsRef.snapshotChanges().pipe(
+      map(changes => 
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    );
+
+    this.projects$.subscribe(result => {this.projects = result; console.log(this.projects)});
   }
 }
 
