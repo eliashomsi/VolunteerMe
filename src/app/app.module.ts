394662fea5f6c100df3a5ea0a911e510e4c1201e
eliastam <elias.tamraz@mail.mcgill.ca>
2020-01-25 07:53:07 -0800

    add profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProjectListComponent } from './project-list/project-list.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   imports: [
@@ -18,7 +19,8 @@ import { ProjectListComponent } from './project-list/project-list.component';
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProjectListComponent
+    ProjectListComponent,
+    ProfileComponent
   ],
   bootstrap: [ AppComponent ]
 })
