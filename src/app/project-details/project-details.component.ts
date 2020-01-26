ac8e5d25c9fc67b8555d4765b37a67ce5de47a1d
George Kandalaft <georgekandalaft@Georges-MacBook-Pro.local>
2020-01-25 21:07:06 -0500

    Finalllyyy

--- src/app/project-details/project-details.component.ts
+++ src/app/project-details/project-details.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectModel} from '../core/project.model'
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-project-details',
   templateUrl: './project-details.component.html',
-  styleUrls: ['./project-details.component.css']
+  styleUrls: ['./project-details.component.scss']
+
 })
-export class ProjectDetailsComponent implements OnInit {
+export class ProjectDetailsComponent  {
+  name="george";
+  tag=[]
+  projectForm: FormGroup;
+  public projectsRef: AngularFireList<ProjectModel>;
+  public db: AngularFireDatabase;
+  public tags: string[];
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+   }
 
-  constructor() { }
+   createForm() {
+    this.projectForm = this.fb.group({
+      name: ['', Validators.required ],
+      address: ['',Validators.required],
+      imageURL: ['',Validators.required],
+      date: ['',Validators.required],
+      volunteers:['',Validators.required],
+      description:['',Validators.required],
+      tag:[[],Validators.required],
+      phoneNumber:['',Validators.required]
+    });
+  }
+  addProject(value){
+    value.tag = this.tag;
+    console.log(value);
 
-  ngOnInit() {
+  }
+  addtag(value){
+   
+    this.tag.push(value.tag)
+    console.log(this.tag);
   }
 
 }
