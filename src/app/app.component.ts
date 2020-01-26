30a0afdeca12b19df0bcc9a7e2732e83b36bede9
George Kandalaft <georgekandalaft@Georges-MacBook-Pro.local>
2020-01-26 04:28:51 -0500

    Dynamic Backgrounds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,19 @@ import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  backgroundURL=['url(https://images.unsplash.com/photo-1509664158680-07c5032b51e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80)',
+  "url(https://images.unsplash.com/photo-1472289065668-ce650ac443d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80)",
+  "url(https://images.unsplash.com/photo-1436262513933-a0b06755c784?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1651&q=80)",
+"url(https://images.unsplash.com/photo-1552154357-aaba12e68e64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80)","url(https://images.unsplash.com/photo-1471286274405-579f8d7132d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80)"]
+  setMyStyles() {
+    
+    let styles = {
+      'background-image': this.backgroundURL[Math.floor(Math.random() * Math.floor(5))],
+      // Math.floor(Math.random() * Math.floor(5))
+    };
+    return styles;
+  }
+  
   public projects: AngularFireList<ProjectModel>;
   public enrollments: AngularFireList<EnrollmentModel>;
 
