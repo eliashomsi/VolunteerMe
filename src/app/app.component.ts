87d4d2f1a97f6ca742c0027a7fcff0652aff7d10
Caren <karen.rizk1997@gmail.com>
2020-01-26 08:47:44 -0500

    changed background picture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,14 +20,14 @@ export class AppComponent {
   "url(https://images.unsplash.com/photo-1436262513933-a0b06755c784?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1651&q=80)",
 "url(https://images.unsplash.com/photo-1552154357-aaba12e68e64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80)",]
 */
-  setMyStyles() {
+  // setMyStyles() {
     
-    let styles = {
-      'background-image': "url(https://images.unsplash.com/photo-1471286274405-579f8d7132d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80)",
-
-    };
-    return styles;
-  }
+  //   let styles = {
+  //     'background-image': "url(https://images.unsplash.com/photo-1471286274405-579f8d7132d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80)",
+  //     'height': '100%'
+  //   };
+  //   return styles;
+  // }
   
   public projects: AngularFireList<ProjectModel>;
   public enrollments: AngularFireList<EnrollmentModel>;
